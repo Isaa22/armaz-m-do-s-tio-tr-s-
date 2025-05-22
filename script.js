3f39ed079b645d06cb419cf26cf2e8b113f7ec60
Maria Isabely <m.carvalho25@escola.pr.gov.br>
2025-05-21 23:31:14 -0300

    dfgh

--- script.js
+++
@@ -1,9 +0,0 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('sw.js').then(reg => {
-      console.log('Service Worker registrado com sucesso:', reg.scope);
-    }).catch(err => {
-      console.error('Falha ao registrar o Service Worker:', err);
-    });
-  });
-}
